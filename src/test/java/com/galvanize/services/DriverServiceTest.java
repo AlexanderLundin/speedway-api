e3f517c74e484f81afdd7f6ea0e47b45814af225
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-04-02 20:35:51 -0700

    reconfigured project to use h2 in memory database
    all tests pass
    
    no database connection required

--- src/test/java/com/galvanize/services/DriverServiceTest.java
+++ src/test/java/com/galvanize/services/DriverServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
@@ -20,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class DriverServiceTest {
 
 
